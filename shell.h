eb6bf627e59fffee4aee13dfb8dc80c9d9b29df2
Juliana-1234 <jhulyanaaggrey@gmail.com>
2022-09-07 08:41:17 -0700

    shell

--- shell.h
+++ shell.h
@@ -1,15 +1,5 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
-
-int prompt(void);
-void _getline(char *);
-int _fork();
 
 #endif
