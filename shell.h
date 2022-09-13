0d6f0918b6cb28decd62bc6ba209a56adf173636
Juliana-1234 <jhulyanaaggrey@gmail.com>
2022-09-13 07:22:11 -0700

    fixing exit

--- shell.h
+++ shell.h
@@ -23,6 +23,8 @@ void freezer(char **token, char *string);
 void _printenv(char **env);
 char **path_finder(char **token_path, char **env_path, char *stringet);
 char *_strcat(char *dest, char *src);
-int childhood(char **token, char *av[], char **env, char *string);
+int childhood(char **token, char *av[], char **env, char *string)i;
+int _strcmp(char *s1, char *s2);
+void shell_exit(char *token);
 
 #endif /* _SHELL_H_ */
